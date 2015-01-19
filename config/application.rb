6edc46e9a86f9dedcd0dc7a17bfa2b3a042f0dfd
Emiliano Liotta <liotta.emiliano@gmail.com>
2015-01-19 17:31:56 -0300

    Arreglo mails heroku

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ module CvOlavarria
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
     config.i18n.default_locale = :es
 
     # Configuración del Mailer. Se utiliza Gmail
